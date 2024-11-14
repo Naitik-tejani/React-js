621c0797ae0f6f0617437804f827d7d826bf2bc9
Naitik-tejani <7133naititejani.com>
2024-11-14 10:27:23 +0530

    add file

---
+++ counterappReact-Redux/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return(
+    <>
+    <div aling="center">
+    <h1>Counter App</h1>
+    <h2>count :- 0</h2>
+     <button >+</button>
+    </div>
+    </>
+  )
+
+ 
+}
+
+export default App
